a89d5ae73e541fad9ceeeaf7728820af6d08ecde
Satadru Roy <satadru.roy98@gmail.com>
2020-07-11 14:26:32 +0530

    Task 1 complete

---
+++ assignment.js
@@ -0,0 +1,13 @@
+const arrNum = [6, 5, 3, 9, 2, 1, 4];
+
+const filteredArray = arrNum.filter(num => num > 5);
+
+console.log(filteredArray);
+
+const mappedArray = arrNum.map(n => ({num: n}));
+
+console.log(mappedArray);
+
+const reducedNumber = arrNum.reduce((sum, curNum) => sum * curNum, 1);
+
+console.log(reducedNumber);
